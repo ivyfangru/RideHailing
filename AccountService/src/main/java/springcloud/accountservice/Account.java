9d9ecca1fc1c1b3cf5e458e53677840ac4b03439
ivyfangru <fangruivy@gmail.com>
2019-01-08 16:05:36 -0800

    Update all

--- AccountService/src/main/java/springcloud/accountservice/Account.java
+++ AccountService/src/main/java/springcloud/accountservice/Account.java
@@ -1,4 +1,45 @@
 package springcloud.accountservice;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Account(){}
+
+    public Account(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Account[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+    @JsonProperty
+    public Long getId() {
+        return id;
+    }
+
+    @JsonProperty
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @JsonProperty
+    public String getLastName() {
+        return lastName;
+    }
+
 }
